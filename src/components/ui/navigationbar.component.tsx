7d0d848a0bb81f14d83892ddaa052572bf09b6ef
Kevin Hudgens <hudgens1073@gmail.com>
2023-01-29 20:22:44 -0600

    created generic navigation bar

---
+++ src/components/ui/navigationbar.component.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+
+export function NavigationBar() {
+  return () => {
+    <div>
+      <Nav
+        activeKey="/home"
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+      >
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </div>;
+  };
+}

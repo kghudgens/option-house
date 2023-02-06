4c8eb80c882c456fca9144e4a09c44718ba8c8c2
Kevin Hudgens <hudgens1073@gmail.com>
2023-02-05 19:33:34 -0600

    Bootstrap nav bar now displayed

--- option-house/src/components/ui/navigationbar.component.tsx
+++ option-house/src/components/ui/navigationbar.component.tsx
@@ -1,23 +1,13 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-export function NavigationBar() {
+export const NavigationBar = () => {
   return (
     <div>
-      <Nav
-        activeKey="/home"
-        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-      >
-        <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
-        </Nav.Item>
-      </Nav>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>Option House</Navbar.Brand>
+      </Navbar>
     </div>
   );
-}
+};

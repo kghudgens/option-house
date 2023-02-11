59171e1d7b6a4f6167b3417a0bd91e70feff0925
Kevin Hudgens <hudgens1073@gmail.com>
2023-02-10 21:37:24 -0600

    Added more styling and links to navbar

--- option-house/src/components/ui/navigationbar.component.tsx
+++ option-house/src/components/ui/navigationbar.component.tsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import "./navigationbar.css";
 
 export const NavigationBar = () => {
   return (
-    <div>
-      <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar bg="light" expand="lg" fixed="top">
         <Navbar.Brand>Option House</Navbar.Brand>
+        <Nav>
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#home">About</Nav.Link>
+          <Nav.Link href="#home">Search</Nav.Link>
+          <Nav.Link href="#home">Your Stocks</Nav.Link>
+        </Nav>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
       </Navbar>
-    </div>
+    </Container>
   );
 };
